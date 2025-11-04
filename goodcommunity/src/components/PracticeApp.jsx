94292336334a1fac5d3a926805977bbbe5b9cb67
PHB-1994 <gudqls0991@naver.com>
2025-11-04 10:20:38 +0900

    react-router-dom

--- goodcommunity/src/components/PracticeApp.jsx
+++ goodcommunity/src/components/PracticeApp.jsx
@@ -1,14 +1,17 @@
-import {useState} from "react";
 import LikeButton from "./practice/day2/LikeButton";
 import TodoInput from "./practice/day2/TodoInput";
+import TemperatureConverter from "./practice/day2/TemperatureConverter";
 
 const PracticeApp = () => {
 
     return (
         <>
             <LikeButton />
+            <TodoInput />
+            <TemperatureConverter />
         </>
     );
 };
 
 export default PracticeApp;
+
--- goodcommunity/src/index.js
+++ goodcommunity/src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import PracticeApp from "./components/PracticeApp";
-
+import Main from "./Main";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
       {/*<App /> */}
-      <PracticeApp />
+      {/*<PracticeApp />*/}
+      <Main />
   </React.StrictMode>
 );
+
+
 
