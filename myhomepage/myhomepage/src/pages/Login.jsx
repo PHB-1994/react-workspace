cd2a4d583bebb544a588fb47a9ade34bbf014e61
PHB-1994 <gudqls0991@naver.com>
2025-11-10 12:02:24 +0900

    react

--- myhomepage/myhomepage/src/pages/Login.jsx
+++ myhomepage/myhomepage/src/pages/Login.jsx
@@ -1,10 +1,32 @@
 // 로그인
+import {useState} from "react";
+
 const Login = () => {
+    const [memberEmail, setMemberEmail] = useState('');
+    const [memberPassword, setMemberPassword] = useState('');
+    const [message, setMessage] = useState('');
+    const [loading, setLoading] = useState(false);
+
+    const handleSubmit = () => {
+
+    }
+
     return (
         <div className="page-container">
-            <h1>로그인</h1>
-            <p>로그인 폼</p>
-
+            <div className="login-box">
+                <h1>로그인</h1>
+                <form onSubmit={handleSubmit}>
+                    <div className="form-group">
+                        <label>이메일
+                            <input type="email"
+                                   id="memberEmail"
+                                   placeholder="이메일을 입력하세요."
+                                   value={}
+                                   onChange={}
+                        </label>
+                    </div>
+                </form>
+            </div>
         </div>
     );
 };
